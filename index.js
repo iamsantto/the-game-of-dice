1de7318f00105f76a2103c060661a2aa6a9dd2f5
Santhosh <santhosh@keepworks.com>
2020-11-05 04:07:10 +0530

    added player turns, prompts and basic scorecard display

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
-(() => {
+(async () => {
+  var readline = require("readline");
+
   var maxPlayers = process.argv[2];
   var maxPoints = process.argv[3];
+  var maxDiceValue = 6;
   var order = [];
-  var players = {};
+  var scorecard = {};
+  var lastThrow = {};
+
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  console.log('Setting up player order...');
+
+  var getRandNumber = (maxValue) => {
+    return Math.floor(Math.random()*(maxValue) + 1);
+  }
 
   while (order.length < maxPlayers) {
-    var number = Math.floor(Math.random()*(maxPlayers) + 1);
+    var number = getRandNumber(maxPlayers);
+
     if (order.indexOf(number) == -1) {
-        order.push(number);
+      order.push(number);
     }
   }
+
+  const playTurn = (player) => {
+    var number = getRandNumber(maxDiceValue);
+
+    console.log('you just rolled a ', number);
+
+    if(player in scorecard) {
+      scorecard[player] += number;
+      lastThrow[player] = number;
+
+      return;
+    }
+
+    scorecard[player] = number;
+    lastThrow[player] = number;
+  }
+
+  const displayScoreCard = () => {
+    console.log(scorecard);
+  }
+
+  var keypress = async (player) => {
+    if (process.stdin.isTTY) process.stdin.setRawMode(true);
+    return new Promise(resolve => process.stdin.once('data', () => {
+      process.stdin.setRawMode(false);
+      playTurn(player);
+      displayScoreCard();
+      resolve();
+    }))
+  }
+
+  for (let index = 0; index < order.length; index++) {
+    console.log('Player-' + order[index] + ' its your turn to roll the dice, press any key to roll.');
+    await keypress(order[index]);
+  }
+
+  rl.close();
 })();
