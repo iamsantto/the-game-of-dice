90340ffa22d9e073a15df49b771e69b75f79588d
Santhosh <santhosh@keepworks.com>
2020-11-05 02:54:36 +0530

    get initial params and generate random order

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 (() => {
+  var maxPlayers = process.argv[2];
+  var maxPoints = process.argv[3];
+  var order = [];
+  var players = {};
 
-
+  while (order.length < maxPlayers) {
+    var number = Math.floor(Math.random()*(maxPlayers) + 1);
+    if (order.indexOf(number) == -1) {
+        order.push(number);
+    }
+  }
 })();
