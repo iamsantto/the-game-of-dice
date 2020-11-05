e4c58d580d169d4c8719feebfae1d2df5d32e49a
Santhosh <santhosh@keepworks.com>
2020-11-05 06:34:54 +0530

    added skip on consecutive 1 feature

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
   var maxPlayers = process.argv[2];
   var maxPoints = process.argv[3];
   var maxDiceValue = 6;
-  var order = [];
+  var minDiceValue = 1;
   var scorecard = {};
-  var lastThrow = {};
+  var skipTracker = {};
+  var rankings = [];
 
   var rl = readline.createInterface({
     input: process.stdin,
@@ -19,47 +20,81 @@
     return Math.floor(Math.random()*(maxValue) + 1);
   }
 
-  while (order.length < maxPlayers) {
-    var number = getRandNumber(maxPlayers);
+  const generatePlayerOrder = () => {
+    var order = {};
 
-    if (order.indexOf(number) == -1) {
-      order.push(number);
+    while (Object.keys(order).length < maxPlayers) {
+      var number = getRandNumber(maxPlayers);
+
+      if (!(number in order) && (order['Player-' + number] = {
+        lastThrow: 0,
+        skip: false
+      }));
     }
+
+    return order;
   }
 
-  const playTurn = (player) => {
+  var playerOrder = generatePlayerOrder();
+
+  const playTurn = async (player) => {
     var number = getRandNumber(maxDiceValue);
+    console.log('You just rolled a ', number + '.');
 
-    console.log('you just rolled a ', number);
+    if (playerOrder[player]['lastThrow'] === minDiceValue && number === minDiceValue) {
+      playerOrder[player]['skip'] = true
+      console.log(player + ': You rolled ' + minDiceValue + ' twice consecutively, you will miss the next turn.')
+    }
 
-    if(player in scorecard) {
-      scorecard[player] += number;
-      lastThrow[player] = number;
+    playerOrder[player]['lastThrow'] = number;
 
+    player in scorecard ? scorecard[player] += number : scorecard[player] = number;
+
+    if (scorecard[player] > maxPoints) {
+      rankings.push(player)
+      delete playerOrder[player];
+
+      console.log('Hurray, ' + player +' won. Your rank is ' + rankings.length + '.')
       return;
     }
 
-    scorecard[player] = number;
-    lastThrow[player] = number;
+    if (number === maxDiceValue) await promptDiceRoll(player);
   }
 
   const displayScoreCard = () => {
-    console.log(scorecard);
+    console.log(playerOrder, 'order');
+    console.log(scorecard, 'scorecard');
+    console.log(rankings, 'leaderboard');
   }
 
-  var keypress = async (player) => {
+  var keypress = async () => {
     if (process.stdin.isTTY) process.stdin.setRawMode(true);
+
     return new Promise(resolve => process.stdin.once('data', () => {
       process.stdin.setRawMode(false);
-      playTurn(player);
-      displayScoreCard();
       resolve();
     }))
   }
 
-  for (let index = 0; index < order.length; index++) {
-    console.log('Player-' + order[index] + ' its your turn to roll the dice, press any key to roll.');
-    await keypress(order[index]);
+  var promptDiceRoll = async (player) => {
+    console.log(player + ': Its your turn to roll the dice, press any key to roll.');
+
+    await keypress().then(async () => {
+      await playTurn(player);
+    });
+  }
+
+  while (rankings.length < maxPlayers) {
+    for (player in playerOrder) {
+      if (playerOrder[player].skip) {
+        console.log(player + 'misses this turn.');
+        playerOrder[player].skip = false;
+        continue;
+      }
+      await promptDiceRoll(player).then(() => {
+        displayScoreCard();
+      });
+    }
   }
 
   rl.close();
